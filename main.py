b2977d1577d02b6c43404fafedc05974698bd14d
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-04-25 21:02:23 -0400

    Parse weather JSON for required data

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 # should check for file failure or invalid format
@@ -7,8 +8,13 @@ key = key_file.readlines()[0]
 
 # should check for request failure
 latlong = requests.get("https://ipinfo.io/loc")
-print(latlong.text)
+# print(latlong.text)
 
 # should check for request failure
-weather = requests.get(f"https://api.weatherapi.com/v1/current.json?key={key}&q={latlong.text}&aqi=no")
-print(weather.text)
+weather_request = requests.get(f"https://api.weatherapi.com/v1/current.json?key={key}&q={latlong.text}&aqi=no")
+# print(weather_request.text)
+
+weather = json.loads(weather_request.text)
+wind_kph = weather["current"]["wind_kph"]
+wind_heading = weather["current"]["wind_degree"]
+print(f"Wind Speed KM/H {wind_kph}, Wind Heading {wind_heading}°")
