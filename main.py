576a5f408bd2a8a0a47711db4926e7f1197960ed
UserC2 <85082371+UserC2@users.noreply.github.com>
2024-04-29 10:50:13 -0400

    Plot vectors as they appear on a compass

--- main.py
+++ main.py
@@ -38,12 +38,12 @@ disc = vector.Vector(disc_kph, disc_heading)
 uncorrected = wind.add(disc)
 
 # ask user if they want a visual before plotting
-if False:
-    rect_wind = wind.as_rect()
-    rect_disc = disc.as_rect()
-    rect_uncorrected = uncorrected.as_rect()
-    plt.quiver([0, 0, 0], [0, 0, 0], [rect_wind[0], rect_disc[0], rect_uncorrected[0]], [rect_wind[1], rect_disc[1], rect_uncorrected[0]], color=["g", "r", "b"], angles="xy", scale_units="xy", scale=1)
-    max = max(max(rect_wind), max(rect_disc), max(rect_uncorrected))
+if True:
+    plotable_wind = wind.as_plotable().as_rect()
+    plotable_disc = disc.as_plotable().as_rect()
+    plotable_uncorrected = uncorrected.as_plotable().as_rect()
+    plt.quiver([0, 0, 0], [0, 0, 0], [plotable_wind[0], plotable_disc[0], plotable_uncorrected[0]], [plotable_wind[1], plotable_disc[1], plotable_uncorrected[0]], color=["g", "r", "b"], angles="xy", scale_units="xy", scale=1)
+    max = max(max(plotable_wind), max(plotable_disc), max(plotable_uncorrected))
     plt.xlim(-max, max)
     plt.ylim(-max, max)
     plt.show()
